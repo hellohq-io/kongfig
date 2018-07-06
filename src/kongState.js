3ea38f54799abcd44a900e37b2ef715d509d731b
Sebastian Roesch <mail@sebastianroesch.de>
2018-07-07 01:02:51 +0200

    Added retry

--- src/kongState.js
+++ src/kongState.js
@@ -1,6 +1,8 @@
 import semVer from 'semver';
 import {getSupportedCredentials} from './consumerCredentials'
 
+var sleep = require('sleep');
+
 const fetchUpstreamsWithTargets = async ({ version, fetchUpstreams, fetchTargets }) => {
     if (semVer.lte(version, '0.10.0')) {
         return Promise.resolve([]);
@@ -26,14 +28,18 @@ const fetchCertificatesForVersion = async ({ version, fetchCertificates }) => {
 };
 
 export default async ({fetchApis, fetchPlugins, fetchGlobalPlugins, fetchConsumers, fetchConsumerCredentials, fetchConsumerAcls, fetchUpstreams, fetchTargets, fetchTargetsV11Active, fetchCertificates, fetchKongVersion}) => {
+    sleep.msleep(100);
     const version = await fetchKongVersion();
+    sleep.msleep(100);
     const apis = await fetchApis();
+    sleep.msleep(100);
     const apisWithPlugins = await Promise.all(apis.map(async item => {
+        sleep.msleep(200);
         const plugins =  await fetchPlugins(item.id);
 
         return {...item, plugins};
     }));
-
+    sleep.msleep(100);
     const consumers = await fetchConsumers();
     const consumersWithCredentialsAndAcls = await Promise.all(consumers.map(async consumer => {
         if (consumer.custom_id && !consumer.username) {
@@ -43,10 +49,12 @@ export default async ({fetchApis, fetchPlugins, fetchGlobalPlugins, fetchConsume
         }
 
         const allCredentials = Promise.all(getSupportedCredentials().map(name => {
+            sleep.msleep(100);
             return fetchConsumerCredentials(consumer.id, name)
                 .then(credentials => [name, credentials]);
         }));
 
+        sleep.msleep(100);
         var aclsFetched = await fetchConsumerAcls(consumer.id);
 
         var consumerWithCredentials = allCredentials
@@ -65,12 +73,15 @@ export default async ({fetchApis, fetchPlugins, fetchGlobalPlugins, fetchConsume
 
     }));
 
+    sleep.msleep(100);
     const allPlugins = await fetchGlobalPlugins();
     const globalPlugins = allPlugins.filter(plugin => {
         return plugin.api_id === undefined;
     });
 
+    sleep.msleep(100);
     const upstreamsWithTargets = await fetchUpstreamsWithTargets({ version, fetchUpstreams, fetchTargets: semVer.gte(version, '0.12.0') ? fetchTargets : fetchTargetsV11Active });
+    sleep.msleep(100);
     const certificates = await fetchCertificatesForVersion({ version, fetchCertificates });
 
     return {
--- src/requester.js
+++ src/requester.js
@@ -1,5 +1,7 @@
 require('isomorphic-fetch');
 
+var sleep = require('sleep');
+
 let headers = {};
 
 const addHeader = (name, value) => { headers[name] = value };
@@ -33,8 +35,20 @@ const request = (uri, opts) => {
     return fetchWithRetry(uri, options);
 };
 
+var requestCounter = 0;
+// var cache = [];
+
 function fetchWithRetry(url, options) {
-  var retries = 3;
+
+//   const cachedResponse = cache.find(c => c.url == url);
+//   if(cachedResponse)
+//   {
+//     return new Promise(function(resolve, reject) {
+//         resolve(cachedResponse.response);
+//     });
+//   }
+
+  var retries = 10;
   var retryDelay = 500;
 
   if (options && options.retries) {
@@ -45,11 +59,32 @@ function fetchWithRetry(url, options) {
     retryDelay = options.retryDelay;
   }
 
+  if(requestCounter % 10 == 0) {
+      sleep.msleep(2000);
+  }
+
   return new Promise(function(resolve, reject) {
     var wrappedFetch = (n) => {
+
+      requestCounter++;
+      console.log(requestCounter + "(" + n + "): " + url);
+
       fetch(url, options)
         .then(response => {
-          resolve(response);
+          if(!response.ok)
+          {
+            if(n <= retries) {
+                setTimeout(() => {
+                    wrappedFetch(n + 1);
+                }, retryDelay * Math.pow(2, n));
+            } else {
+                reject(error);
+            }
+          } else {
+            //   if(!cache.some(c => c.url == url))
+            //     cache.push({ url: url, response: response });
+            resolve(response);
+          }
         })
         .catch(error => {
           if (n <= retries) {
